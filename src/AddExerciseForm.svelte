37cc9a1c726a53742d60e9440fd0b3d502f9ab66
knockoffduck <ddarm4@gmail.com>
2022-08-22 15:41:21 +0800

    new commit

---
+++ src/AddExerciseForm.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let addExercise;
+
+    let newExercise = "";
+
+    function formSubmitted() {
+        addExercise(newExercise);
+        newExercise = "";
+    }
+</script>
+
+<form on:submit|preventDefault={formSubmitted}>
+    <h2>Workout Routine</h2>
+    <label>
+        <input bind:value={newExercise} placeholder="Enter an Exercise..." />
+    </label>
+    <button>Add Exercise</button>
+</form>
---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+	import AddExerciseForm from "./AddExerciseForm.svelte";
+
+	const exercises = [];
+
+	function addExercise(exercise) {
+		exercises.push({
+			title: exercise,
+			completed: false,
+		});
+
+		exercises = exercises;
+	}
+</script>
+
+<main>
+	<AddExerciseForm {addExercise} />
+	<ul>
+		{#each exercises as exercise}
+			<li
+				class:doneon:click={() =>
+					(exercise.completed = !exercise.completed)}
+			>
+				{exercise.title}
+			</li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.done {
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
